2006af77d40ed19579fc8db86d76d3868fd113ff
KeisukeYamashita <keisuke.yamashita@linecorp.com>
2022-08-24 02:39:29 +0900

    doc: add missing go comments (#7)
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>
    
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

--- bin.go
+++ bin.go
@@ -1,16 +1,22 @@
 package consistent
 
+// Bin represents an entity that serves the balls.
+// Usually it's a server but it can be anything.
 type Bin struct {
 	Name         string
 	PartitionIDs []PartitionID
 }
 
+// NewBin generates a bin from the passed name.
+// In most cases, it's IP address of the server, hash of the metadata, etc.
+// It can be anything, it's up to you.
 func NewBin(name string) Bin {
 	return Bin{
 		Name: name,
 	}
 }
 
+// String returns the bin's name.
 func (b Bin) String() string {
 	return b.Name
 }
