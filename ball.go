af28122007cdccc1b1fb39fa5d49ce3b0998c9c3
KeisukeYamashita <19yamashita15@gmail.com>
2022-08-24 02:35:56 +0900

    doc: add missing go comments
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

--- ball.go
+++ ball.go
@@ -1,5 +1,9 @@
 package consistent
 
+// Ball represents data to be placed on the ring.
+// In most cases, it is a subsequent server for load balancing, data for sharding, etc but
+// it can be anything unless it implements this interface.
 type Ball interface {
+	// String returns the name of the ball.
 	String() string
 }
